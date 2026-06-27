631d35e2ce4d5947d9303b1f042c1dcaaa8c2a5d
Jonathan Beskow <jonathanb.16@hotmail.com>
2026-06-27 10:44:15 -0300

    ending the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import { useState } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages/home'
 import { Admin } from './pages/admin'
+import { ErrorPage } from './pages/error'
+import { Home } from './pages/home'
 import { Login } from './pages/login'
 import { Network } from './pages/networks'
 import { Private } from './routes/Private'
-
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
-  }, 
+  },
   {
     path: '/login',
-    element: <Login/>
+    element: <Login />
   },
   {
     path: '/admin',
-    element: <Private><Admin/></Private>
+    element: <Private><Admin /></Private>
   },
   {
     path: '/admin/social',
-    element: <Private><Network/></Private>
+    element: <Private><Network /></Private>
+  },
+  {
+    path: "*",
+    element: <ErrorPage />
   }
 ])
 
-export {router};
+export { router }
+
